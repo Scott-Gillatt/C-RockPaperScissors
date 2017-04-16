98feedeef8ddecad6469ffa304b7a6584dc8c722
Scott-Gillatt <scottgillatt@gmail.com>
2017-04-15 23:40:36 -0600

    Can Load and Save the game

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -10,43 +10,40 @@ namespace RockPaperScissors
 {
     class Program
     {
-        static int playerWins = 0, computerWins = 0, tieWins = 0, totalPlays = 0;
+        public static int playerWins = 0, computerWins = 0, tieWins = 0, totalPlays = 0;
         static Random r = new Random();
-        static ArrayList runningStats = new ArrayList();
+        static List<string> runningStats = new List<string>();
         static void Main(string[] args)
         {
             ReadAndWriteFile.FileWork.LoadGame();
-            Console.WriteLine("Hello What is your name? ");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name} its nice to meet you!");
-            GamePlay(name);
+            GamePlay();
         }
 
-        private static void GamePlay(string name)
+        private static void GamePlay()
         {
             Console.WriteLine("Lets play a game of Rock, Paper, or Scissors");
             Console.WriteLine("Are you ready? Yes or No ");
             string input = Console.ReadLine().ToLower();
             if (ValidateInput(input))
             {
-                RockPaperScissors(name);
+                RockPaperScissors();
                 while (PlayAgain())
                 {
                     Console.Clear();
-                    RockPaperScissors(name);
+                    RockPaperScissors();
                 }
-                EndGame(name);
+                EndGame();
             }
             else
             {
                 Console.Clear();
-                Console.WriteLine($"Sorry to hear {name}, that you don't want to play.  Maybe next time!");
+                Console.WriteLine($"Sorry that you don't want to play.  Maybe next time!");
                 Console.ReadLine();
             }
 
         }
 
-        private static void RockPaperScissors(string name)
+        private static void RockPaperScissors()
         {
             Console.Clear();
             Console.WriteLine("ROCK ON!! Lets play!");
@@ -56,8 +53,8 @@ namespace RockPaperScissors
             string winner = Winner(playersPick, computerPick);
             Stats(winner);
 
-            Console.WriteLine(DisplayWinner(winner, playersPick, computerPick, name));
-
+            Console.WriteLine(DisplayWinner(winner, playersPick, computerPick));
+            runningStats.Add($"PlayerWins:{playerWins},ComputerWins:{computerWins},Ties:{tieWins},TotalPlays:{totalPlays};");
             Console.WriteLine($"\nPlayer Wins: {playerWins}, Computer Wins: {computerWins}, and Ties: {tieWins}, out of {totalPlays}");
 
         }
@@ -73,10 +70,11 @@ namespace RockPaperScissors
 
         }
 
-        private static void EndGame(string name)
+        private static void EndGame()
         {
+            ReadAndWriteFile.FileWork.SaveGame(runningStats);
             Console.Clear();
-            Console.WriteLine($"Ok, Bye Bye {name}, See you Next Time!!");
+            Console.WriteLine($"Ok, Bye Bye. See you Next Time!!");
             Console.ReadLine();
         }
 
@@ -84,11 +82,25 @@ namespace RockPaperScissors
         {
             switch (theirPick.ToLower())
             {
-                case "r": case "rock": case "ro": case "roc":
+                case "r":
+                case "rock":
+                case "ro":
+                case "roc":
                     return "Rock";
-                case "p": case "pa": case "pap": case "pape": case "paper":
+                case "p":
+                case "pa":
+                case "pap":
+                case "pape":
+                case "paper":
                     return "Paper";
-                case "s": case "sc": case "sci": case "scis": case "sciss": case "scisso": case "scissor": case "scissors":
+                case "s":
+                case "sc":
+                case "sci":
+                case "scis":
+                case "sciss":
+                case "scisso":
+                case "scissor":
+                case "scissors":
                     return "Scissors";
                 default:
                     Console.WriteLine($"Sorry, {theirPick} is a invalid input.");
@@ -120,12 +132,12 @@ namespace RockPaperScissors
                 return "computer";
         }
 
-        private static string DisplayWinner(string winner, string playersPick, string computersPick, string name)
+        private static string DisplayWinner(string winner, string playersPick, string computersPick)
         {
             if (winner == "player")
-                return $"Great job {name} you win!!! You picked {playersPick} and the Computer picked {computersPick}";
+                return $"Great job you win!!! You picked {playersPick} and the Computer picked {computersPick}";
             else if (winner == "computer")
-                return $"Sorry {name}, Computer Won.. It picked {computersPick} and you picked {playersPick}";
+                return $"Sorry, Computer Won.. It picked {computersPick} and you picked {playersPick}";
             else
                 return "Games Result was a Tie.  Try Again.";
         }
@@ -161,24 +173,74 @@ namespace ReadAndWriteFile
 {
     class FileWork
     {
+        static string filePath = $@"C:\Users\{Environment.UserName}\Desktop\RockPaperScissors.txt";
         public static void LoadGame()
         {
-            string filePath = $@"C:\Users\{Environment.UserName}\Desktop\RockPaperScissors.txt";
             if (File.Exists(filePath))
             {
-                using (StreamWriter file = new StreamWriter(filePath, true))
-                {
-                    Console.WriteLine("Please Enter in what you want to write to the file?");
-                    string TEST = Console.ReadLine();
-                    file.WriteLine(TEST);
-                }
+                string[] lines = File.ReadAllLines(filePath);
+                string lastLine = lines[lines.Length - 1];
 
+                List<int> lastStats = new List<int>();
+                //for (int x = 0; x < lastLine.Length; x++)
+                //{
+                //    string total = "";
+                //    if ((lastLine[x] > 47) && lastLine[x] < 58)
+                //    {
+
+                //        Console.Write($"{lastLine[x]} is a Integer ");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine($"{lastLine[x]} is a string");
+                //    }
+                //}
+                for (int x = 0; x < lastLine.Length; x++)
+                {
+                    if (lastLine[x] == 58)
+                    {
+                        StringBuilder temp = new StringBuilder();
+                        for (int j = x + 1; j < lastLine.Length + 1; j++)
+                        {
+                            if ((lastLine[j] == 44) || (lastLine[j] == 59))
+                            {
+                                lastStats.Add(Convert.ToInt32(temp.ToString()));
+                                x += (j - x);
+                                break;
+                            }
+                            else
+                            {
+                                temp.Append(lastLine[j]);
+                            }
+                        }
+                    }
+                }
+                RockPaperScissors.Program.playerWins = lastStats[0];
+                RockPaperScissors.Program.computerWins = lastStats[1];
+                RockPaperScissors.Program.tieWins = lastStats[2];
+                RockPaperScissors.Program.totalPlays = lastStats[3];
             }
             else
             {
-                using (StreamWriter sw = File.CreateText(filePath))
+                CreateGame();
+            }
+        }
+
+        private static void CreateGame()
+        {
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine("PlayerWins:0,ComputerWins:0,Ties:0,TotalPlays:0;");
+            }
+        }
+
+        public static void SaveGame(List<string> Data)
+        {
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                foreach(string data in Data)
                 {
-                    sw.WriteLine("Player Wins: 0, Computer Wins: 0, Ties: 0, TotalPlays: 0");
+                    sw.WriteLine(data);
                 }
             }
         }
